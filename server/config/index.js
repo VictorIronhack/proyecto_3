15e66ace0894901d859aac2f7038ce15fd863c1d
Víctor M <victormaraver@me.com>
2021-10-07 13:54:11 +0200

    deploy

--- server/config/index.js
+++ server/config/index.js
@@ -16,13 +16,13 @@ module.exports = (app) => {
   // Because this is a server that will accept requests from outside and it will be hosted ona server with a `proxy`, express needs to know that it should trust that setting.
   // Services like heroku use something called a proxy and you need to add this to your server
   app.set("trust proxy", 1);
-  app.use(express.static(path.join(__dirname, "..","public")))
   
   // In development environment the app logs
   app.use(logger("dev"));
-
+  
   // To have access to `body` property in the request
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
   app.use(cookieParser());
+  app.use(express.static(path.join(__dirname, "..","public")))
 };
