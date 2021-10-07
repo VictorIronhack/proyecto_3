5bd093433afbe2924cae710f8efb8899e4611f70
Víctor M <victormaraver@me.com>
2021-10-07 11:42:23 +0200

    deploy

--- server/app.js
+++ server/app.js
@@ -6,17 +6,19 @@ require("./db");
 
 const express = require("express");
 
+const path = require('path')
+
 const app = express();
 
 require("./config")(app);
 require("./config/session.config")(app)
 require("./config/cors.config")(app);
 
-
+app.use(express.static(path.join(__dirname, '..', "public")))
 
 const allRoutes = require("./routes");
 app.use("/api", allRoutes);
 
-require("./error-handling")(app);
+app.use((req, res) => res.sendFile(__dirname + "/public/index.html"));
 
 module.exports = app;
